b3a5c0fb86b93ccbeaca024c3308831234f4cba7
Domenico Natale <dnatale731@gmail.com>
2021-01-03 20:21:40 -0500

    00 - 05

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,13 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  let editedTitle = title.toLowerCase()
+  
+  editedTitle = editedTitle.split(" ")
+  for(let i = 0; i < editedTitle.length; i++) {
+    editedTitle[i] = editedTitle[i].charAt(0).toUpperCase() + editedTitle[i].slice(1);
+  }
+  
+  return editedTitle.join(" ")
 }
 
 // Do not edit this line;
